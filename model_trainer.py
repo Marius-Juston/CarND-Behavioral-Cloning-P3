8e0b6a6310603dfd525773b2d8803bc25a9fcdee
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-03 12:53:54 -0400

    Uses generator to train model

--- model_trainer.py
+++ model_trainer.py
@@ -1,12 +1,16 @@
 import csv
 import os
 from datetime import datetime
+from math import ceil
 
 import cv2
 import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.callbacks import EarlyStopping, TensorBoard
 from tensorflow.python.keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D, BatchNormalization
 from tensorflow.python.keras.models import Model, Sequential
+from tensorflow.python.training.adam import AdamOptimizer
 
 
 def create_model():
@@ -26,6 +30,7 @@ def create_model():
     model.add(Flatten())
     model.add(Dense(1164, activation='relu', kernel_initializer='he_uniform'))
     model.add(BatchNormalization())
+
     model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
     model.add(BatchNormalization())
     model.add(Dense(50, activation='relu', kernel_initializer='he_uniform'))
@@ -52,7 +57,35 @@ def get_image(line, index, array, separator=os.sep, image_directory='data/IMG/')
     array.append(cv2.flip(image, 1))
 
 
-def load_images(driving_log_file, image_dir):
+def generator(samples, driving_log_file, batch_size=64):
+    num_samples = len(samples)
+    image_dir = driving_log_file.split('/')[0] + "/IMG/"
+
+    while 1:  # Loop forever so the generator never terminates
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset + batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = image_dir + batch_sample[0].split(os.sep)[-1]
+                center_image = cv2.imread(name)
+                center_angle = batch_sample[1]
+
+                if batch_sample[2]:
+                    center_image = cv2.flip(center_image, 1)
+
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+
+            yield tuple(sklearn.utils.shuffle(X_train, y_train))
+
+
+def load_images(driving_log_file, batch_size=64, correction=.2):
     lines = []
 
     with open(driving_log_file) as csv_file:
@@ -60,54 +93,50 @@ def load_images(driving_log_file, image_dir):
         next(reader, None)
 
         for line in reader:
-            lines.append(line)
+            steering_center = float(line[3])
+            steering_right = steering_center - correction
+            steering_left = steering_center + correction
 
-    center = []
-    left = []
-    right = []
-    measurements = []
+            lines.append((line[0], steering_center, 0))
+            lines.append((line[0], steering_center, 1))
+            lines.append((line[1], steering_left, 0))
+            lines.append((line[1], steering_left, 1))
+            lines.append((line[2], steering_right, 0))
+            lines.append((line[2], steering_right, 1))
 
-    for line in lines:
-        if float(line[6]) >= 25:
-            get_image(line, 0, center, image_directory=image_dir)
-            get_image(line, 1, left, image_directory=image_dir)
-            get_image(line, 2, right, image_directory=image_dir)
+    train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-            measurement = float(line[3])
-            measurements.append(measurement)
-            measurements.append(-measurement)
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, driving_log_file, batch_size=batch_size)
+    validation_generator = generator(validation_samples, driving_log_file, batch_size=batch_size)
 
-    return np.array(center), np.array(left), np.array(right), np.array(measurements)
+    return train_generator, validation_generator, len(train_samples), len(validation_samples)
 
 
 if __name__ == '__main__':
     image_file = 'data/driving_log.csv'
-    save_images = 'images.npz'
-
-    if os.path.exists(save_images):
-        print("Loading")
-        data = np.load(save_images, allow_pickle=True)
-        images, measurements, left, right = data['images'], data['measurements'], data['left'], data['right']
-    else:
-        images, left, right, measurements = load_images(image_file, 'data/IMG/')
-        np.savez(save_images, images=images, measurements=measurements, left=left, right=right)
 
     early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, restore_best_weights=True, patience=5)
 
     model = create_model()
     model.summary()
 
-    model.compile(loss='mse', optimizer='adam')
+    model.compile(loss='mse', optimizer=AdamOptimizer(0.0001))
 
     date_time = datetime.now().strftime("%Y%m%d-%H%M%S")
 
     logdir = "logs/fit/" + date_time
     tensorboard_callback = TensorBoard(log_dir=logdir, histogram_freq=1)
 
-    inputs = np.concatenate((left, images, right))
-    outputs = np.concatenate((measurements, measurements, measurements))
+    batch_size = 64
 
-    model.fit(inputs, outputs, shuffle=True, validation_split=.2, epochs=4,
-              callbacks=[early_stop, tensorboard_callback], batch_size=64)
+    train_generator, validation_generator, l_train_samples, l_validation_samples = load_images(image_file, batch_size)
+    print(l_train_samples, l_validation_samples)
+
+    model.fit(train_generator,
+              steps_per_epoch=ceil(l_train_samples / batch_size),
+              validation_data=validation_generator,
+              validation_steps=ceil(l_validation_samples / batch_size),
+              epochs=5, verbose=1, callbacks=[early_stop, tensorboard_callback])
 
     model.save(f'model{date_time}.h5')
